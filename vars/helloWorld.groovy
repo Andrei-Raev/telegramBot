3dbd3133f46eb6073f8945f0a59a73694b24ddb8
Andrei-Raev <51417538+Andrei-Raev@users.noreply.github.com>
2024-07-26 18:11:10 +0300

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,9 @@
+package vars
+
+import org.telebot.TelegramBot
+
 def call(Map config = [:]) {
+    TelegramBot telegramBot = new TelegramBot(env.TELEGRAM_CHAT_ID, env.TELEGRAM_TOKEN)
+
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 }
